dba201856d4927d454319d761651746794508374
Romain1309 <romain.gardet.1@ulaval.ca>
2025-05-09 09:51:17 -0400

    Fixed issue with contact me section + made responsive

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -20,10 +20,10 @@ export const Contact = () => {
         import.meta.env.VITE_PUBLIC_KEY
       )
       .then((result) => {
-        alert("Message sent !");
+        alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
       })
-      .catch(() => alert("Oops something went wrong. Please try again."));
+      .catch(() => alert("Oops! Something went wrong. Please try again."));
   };
 
   return (
@@ -32,8 +32,9 @@ export const Contact = () => {
       className="min-h-screen flex items-center justify-center py-20"
     >
       <RevealOnScroll>
-        <div className="px-4 w-150">
+        <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
           <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-purple-500 to-pink-600 bg-clip-text text-transparent text-center">
+            {" "}
             Contact Me
           </h2>
           <form className="space-y-6" onSubmit={handleSubmit}>
@@ -75,12 +76,13 @@ export const Contact = () => {
                 rows={5}
                 value={formData.message}
                 className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-purple-500 focus:bg-purple-500/5"
-                placeholder="Type your message here..."
+                placeholder="Your Message..."
                 onChange={(e) =>
                   setFormData({ ...formData, message: e.target.value })
                 }
               />
             </div>
+
             <button
               type="submit"
               className="w-full bg-purple-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
