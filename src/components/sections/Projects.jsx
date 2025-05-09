a03144f28f3911899b2610af9f6a0d0da936f5de
Romain1309 <romain.gardet.1@ulaval.ca>
2025-05-08 22:47:45 -0400

    Updated projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -14,68 +14,6 @@ export const Projects = () => {
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-              <h3 className="text-xl font-bold mb-2">Name of projects 1</h3>
-              <p className="text-gray-400 mb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Veritatis ullam harum exercitationem, adipisci suscipit voluptas
-                officia, omnis neque provident quas obcaecati tenetur at. Vel,
-                laudantium voluptates assumenda illum nihil in.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 
-                                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                                "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="google.com"
-                  className="text-purple-400 hover:text-purple-300 transition-colors my-4"
-                >
-                  {" "}
-                  View Project →{" "}
-                </a>
-              </div>
-            </div>
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
-              <h3 className="text-xl font-bold mb-2">Name of projects 1</h3>
-              <p className="text-gray-400 mb-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Veritatis ullam harum exercitationem, adipisci suscipit voluptas
-                officia, omnis neque provident quas obcaecati tenetur at. Vel,
-                laudantium voluptates assumenda illum nihil in.
-              </p>
-              <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "AWS", "Docker"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 
-                                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                                "
-                  >
-                    {tech}
-                  </span>
-                ))}
-              </div>
-
-              <div className="flex justify-between items-center">
-                <a
-                  href="google.com"
-                  className="text-purple-400 hover:text-purple-300 transition-colors my-4"
-                >
-                  {" "}
-                  View Project →{" "}
-                </a>
-              </div>
-            </div>
-            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
               <h3 className="text-xl font-bold mb-2">TryHackMe Training</h3>
               <p className="text-gray-400 mb-4">
                 Ongoing training on real-world cybersecurity scenarios:
@@ -89,7 +27,7 @@ export const Projects = () => {
                   "Nmap",
                   "Reverse Engineering",
                   "OSINT",
-                  "Pentest"
+                  "Pentest",
                 ].map((tech, key) => (
                   <span
                     key={key}
@@ -113,6 +51,39 @@ export const Projects = () => {
                   className="text-purple-400 hover:text-purple-300 transition-colors"
                 >
                   View Profile →
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+              <h3 className="text-xl font-bold mb-2">Name of projects 1</h3>
+              <p className="text-gray-400 mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis ullam harum exercitationem, adipisci suscipit voluptas
+                officia, omnis neque provident quas obcaecati tenetur at. Vel,
+                laudantium voluptates assumenda illum nihil in.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Skills1", "Skills2", "Skills3", "Skills4"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 
+                                                hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                                "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="google.com"
+                  className="text-purple-400 hover:text-purple-300 transition-colors my-4"
+                >
+                  {" "}
+                  View Project →{" "}
                 </a>
               </div>
             </div>
